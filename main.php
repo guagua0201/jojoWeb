623835a42dc6b9c2961fb65114f73952606da9c5
guagua0201 <tobyupu@gmail.com>
2019-10-15 21:03:10 +0800

    bug in main.php , test it

--- main.php
+++ main.php
@@ -1,9 +1,9 @@
 <title>動物特徵查詢網</title>
 
 <link rel="icon" href="/images/ladybug.ico">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
@@ -28,7 +28,7 @@
 			$sql = "SELECT * FROM `Animal` OFFSET ".$nowPage*10." LIMIT 10";
 		}
 		else{
-			$sql = "SELECT * FROM `Animal` WHERE `Level` = ".$level." AND `Name` = \"".$name."\" OFFSET ".$nowPage*10." LIMIT 10";
+			//$sql = "SELECT * FROM `Animal` WHERE `Level` = ".$level." AND `Name` = \"".$name."\" OFFSET ".$nowPage*10." LIMIT 10";
 		}
 
 		if($result = mysqli_query($link, $sql)) {
