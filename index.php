6453ea19ff7eef95784dd3c736907405caba029f
guagua0201 <tobyupu@gmail.com>
2019-10-15 23:13:26 +0800

    fix index search also

--- index.php
+++ index.php
@@ -22,10 +22,10 @@
 				<button type="button" class="btn btn-secondary" onclick="location.href='chooseCategory.php';">選擇分類</button>
 			</div>
 			
-			<div class="col-5" style="text-align:right;">
+			<div class="col-5" style="text-align:right;" action="searchResult.php" method="get">
 				<form class="form-inline active-cyan-4">
 				  <input class="form-control form-control-sm mr-3 w-75" type="text" placeholder="Search"
-				    aria-label="Search" action="searchResult.php" method="get">
+				    aria-label="Search" name="keyword">
 				</form>
 			</div>
 		</div>
