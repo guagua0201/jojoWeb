d223f8c21a4083509a3ae7b5aa9c4d93d1261a92
guagua0201 <tobyupu@gmail.com>
2019-10-15 20:25:22 +0800

    add round rect

--- index.php
+++ index.php
@@ -15,9 +15,9 @@
 
 <!-- body start from here -->
 <body style="background-color:gray;">
-	<div class="container" style="background-color:white;">
+	<div class="container" style="background-color:white;border:7%;border-radius:10%;">
 		<div class = "row">
-			<<button type="button" class="btn btn-secondary">選擇分類</button>
+			<button type="button" class="btn btn-secondary">選擇分類</button>
 			<form class="form-inline active-cyan-4">
 			  <input class="form-control form-control-sm mr-3 w-75" type="text" placeholder="Search"
 			    aria-label="Search">
